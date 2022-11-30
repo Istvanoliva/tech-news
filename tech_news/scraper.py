6690caf9d970a29c312b090639f115618ddf079a
Istvan <olivaistvan@gmail.com>
2022-11-30 18:33:07 -0300

    feat: (scraper) create scrape_noticia()

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,20 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    return {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("span.author a.url.fn.n::text").get(),
+        "comments_count": len(selector.css("ol.comment-list li").getall()),
+        "summary": "".join(
+            selector.css("div.entry-content > p:nth-of-type(1) *::text")
+            .getall()).strip(),
+        "tags": selector.css("a[rel=tag]::text").getall(),
+        "category": selector.css("span.label::text").get()
+    }
 
 
 # Requisito 5
