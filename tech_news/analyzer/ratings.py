924cf7bb9489cbd9dd165dfbc737a5c0e4f37669
Istvan <olivaistvan@gmail.com>
2022-12-01 19:45:42 -0300

    feat: (rantings) create top_5_news()

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import get_collection
+from tech_news.analyzer.helpers import categorize_list
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = get_collection()
+    
+    result = (
+        news_list.find({})
+        .sort([("comments_count", -1), ("title", 1)])
+        .limit(5)
+    )
+
+    return categorize_list(result)
 
 
 # Requisito 11
